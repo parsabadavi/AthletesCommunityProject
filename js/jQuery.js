f3d8a811d1053c391c9e7ab585de0902a456752c
parsa badavi <seyed.parsa1@gmail.com>
2023-11-08 13:54:59 +0330

    End FlipCards & Start Main

--- js/jQuery.js
+++ js/jQuery.js
@@ -8,38 +8,36 @@ $(document).ready(function () {
     duration: 1200,
     delay: 300,
   });
-// counter
-var a = 0;
-$(window).scroll(function () {
-  var oTop = $("#counter").offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $(".counter-value").each(function () {
-      var $this = $(this),
-        countTo = $this.attr("data-count");
-      $({
-        countNum: $this.text(),
-      }).animate(
-        {
-          countNum: countTo,
-        },
-
-        {
-          duration: 3000,
-          easing: "swing",
-          step: function () {
-            $this.text(Math.floor(this.countNum));
+  // counter
+  var a = 0;
+  $(window).scroll(function () {
+    var oTop = $("#counter").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $(".counter-value").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
           },
-          complete: function () {
-            $this.text(this.countNum);
-          },
-        }
-      );
-    });
-    a = 1;
-  };
+
+          {
+            duration: 3000,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
+          }
+        );
+      });
+      a = 1;
+    }
+  });
+  //activeFlipCard
+  $(".activeFlipCard")
 });
-});
-
-
-
-
